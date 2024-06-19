98ae4e2d2c6a97f56e938a7383eeaaab8e1763b0
Giles Dring <giles@dringtech.com>
2024-06-19 19:15:50 +0100

    Add prism highlighter

--- _config.ts
+++ _config.ts
@@ -1,9 +1,32 @@
 // Check deno.json for import maps - this is where versions of modules are specified.
 import lume from "lume/mod.ts";
+import postcss from "lume/plugins/postcss.ts";
+import prism from "lume/plugins/prism.ts";
+
+/**
+ * Import languages
+ */
+import "npm:prismjs/components/prism-sql.js";
+import "npm:prismjs/components/prism-typescript.js";
 
 const site = lume({
   // Set the source directory
-  src: "src"
+  src: "src",
 });
+
+/**
+ * Setup postcss processor
+ */
+site.use(postcss());
+
+/**
+ * Setup code highlighting
+ */
+site.use(prism({
+  theme: {
+    name: 'okaidia',
+    path: "/_includes/css/code_theme.css"
+  }
+}));
 
 export default site;
