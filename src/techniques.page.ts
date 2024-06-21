55147d3c8aed21572faaf9d29781d02b0376742b
Giles Dring <giles@dringtech.com>
2024-06-21 08:34:30 +0100

    Simplify techniques page generation

--- src/techniques.page.ts
+++ src/techniques.page.ts
@@ -1,20 +1,53 @@
-export const layout = "layout/catalogue.vto";
+/**
+ * A generator which creates a page per technique in use.
+ *
+ * @param pageData Lume page data
+ */
+export default function* ({ search, techniques }: Lume.Data & {
+  techniques: Technique[];
+}) {
+  // Find all values of technique in use
+  const keys = search.values<string>("technique");
 
-export const tags = ["technique"];
+  for (const key of keys) {
+    // Search for the technique in the techniques site data (see /src/_data.yml)
+    const technique = techniques.find((t) => t.key === key);
+    if (!technique) {
+      throw new Error(`Technique ${key} not defined in _data.yml`);
+    }
 
-const capitalise = (s: string) => s.charAt(0).toUpperCase() + s.slice(1);
+    // Find the technique list position
+    const order = techniques.indexOf(technique);
 
-export default function* ({ search }: Lume.Data) {
-  const techniques = search.values<string>("technique");
-
-  for (const technique of techniques) {
-    const pages = search.pages(`technique=${technique}`).map(
+    // Find all pages that have this technique key set
+    // Sort by title
+    const pages = search.pages(`technique=${key}`, 'title').map(
       ({ title, url }) => ({ title, url }),
     );
+
+    // Yield the page data for this page.
+    // This will be used in the layout defined below to create the page.
+    // `order` is used for lists of the content
     yield {
-      title: capitalise(technique),
-      url: `/${technique}/`,
+      ...technique,
+      order,
+      url: `/${key}/`,
       catalogue: pages,
     };
   }
 }
+
+/**
+ * Definition of a technique entry
+ */
+type Technique = { key: string; title: string };
+
+/**
+ * All pages created by this generator will use the catalogue layout
+ */
+export const layout = "layout/catalogue.vto";
+
+/**
+ * All pages created by this generator will be tagged `technique`
+ */
+export const tags = ["technique"];
